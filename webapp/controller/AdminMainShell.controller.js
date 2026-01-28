384dd366aef290873a33ca423d97caf8c333e360
RickHackUrSht <ngyuhin2004@gmail.com>
2026-01-28 22:45:14 +0800

    admin

---
+++ webapp/controller/AdminMainShell.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("project1.controller.AdminMainShell", {
+        onInit() {
+        }
+    });
+});
+
+
